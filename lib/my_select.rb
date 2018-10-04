8bb9b71d85d2bcc6d07b37d80990cd005307c610
scottyd_14 <scottyd_14@hotmail.com>
2018-10-04 22:34:56 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(collection)
  i = 0 
  result = []
+ 
  while i < collection.length 
     if yield(collection[i]) == true
       result<< collection[i]
- 
-    end
+     end
   i+=1 
   end
   result
